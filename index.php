11ae274a03b9da82652529bc575f2ee87ccf7a8a
Oksana-Tnt <oksanatantsiura@gmail.com>
2025-01-09 22:23:54 +0100

    create order form

---
+++ index.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Order</title>
+</head>
+
+<body>
+    <?php
+    $pizze = [
+        [
+            'gusto' => 'Margherita',
+            'prezzo' => 5
+        ],
+        [
+            'gusto' => 'Diavola',
+            'prezzo' => 1
+        ],
+        [
+            'gusto' => 'Marinara',
+            'prezzo' => 4
+        ],
+    ];
+    ?>
+    <form action="output.php" method="POST">
+        <fieldset>
+            <legend>Personal information:</legend>
+            <label for="fname">First name:</label><br>
+            <input type="text" id="fname" name="fname" placeholder="Enter your first name"><br>
+            <label for="lname">Last name:</label><br>
+            <input type="text" id="lname" name="lname" placeholder="Enter your last name"><br>
+            <label for="address">Address:</label><br>
+            <input type="text" id="address" name="address" placeholder="Enter your address"><br>
+            <input type="submit" value="Submit">
+        </fieldset>
+
+        <label for="pizza">Pizza</label>
+        <?php
+        echo '<select name="pizza" id="pizza">';
+        foreach ($pizze as $pizza) {
+            echo '<option value="' . $pizza['gusto'] . $pizza['prezzo'] . '">' . $pizza['gusto'] . ' (' . $pizza['prezzo'] . '€)</option>';
+        }
+        echo '</select>';
+        ?>
+
+        <label for="pizze">Quantity</label>
+        <input type="number" id="quantity" name="quantity" value="1">
+
+        <button>Order</button>
+    </form>
+</body>
+
+</html>
---
+++ output.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Grazie per aver inviato il messaggio</title>
+</head>
+
+<body>
+
+    <?php
+
+    if (!isset($_POST['fname']) || !isset($_POST['lname']) || !isset($_POST['address']) || !isset($_POST['pizza']) || !isset($_POST['quantity'])) {
+        header('Location: index.php');
+        exit;
+    }
+
+    $fname = $_POST['fname'];
+    $lname = $_POST['lname'];
+    $address = $_POST['address'];
+    $pizza = substr($_POST['pizza'], 0, -1);
+    $quantity = $_POST['quantity'];
+    $total = intval(substr($_POST['pizza'], -1)) * intval($quantity);
+
+    if (empty($fname) || empty($lname) || empty($address) || empty($pizza) || empty($quantity)) {
+        echo 'Devi compilare il form';
+        echo '<script>
+                    setTimeout(()=>{
+                        location.href = "index.php";
+                    },5000)
+                </script>';
+        exit;
+    }
+
+    $file = __DIR__ . '/orders/.order.txt';
+    $fileContent = "Customer: $fname $lname" . "\r\n" . "Address: $address" . "Order: \r\n" . "Pizza: $pizza" . "\r\n" . "Quantity: $quantity" . "\r\n" . "Total amount: $total" . "\r\n";
+
+    file_put_contents($file, $fileContent);
+    echo "Dear $fname $lname,<br> we're successfully received your order!<br> Your order: <br> Pizza: $pizza <br>Quantity: $quantity <br> Total amount: $total";
